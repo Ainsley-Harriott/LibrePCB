2e7493354b2bb6f4bef4c4e39de4fb1c58a0c1f3
Danilo Bargen <mail@dbrgn.ch>
2018-05-12 20:52:00 +0200

    Add simple version of new about dialog

---
+++ libs/librepcb/common/dialogs/aboutdialog.h
@@ -0,0 +1,24 @@
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include <QDialog>
+#include <QAbstractButton>
+#include "../version.h"
+
+namespace Ui {
+class AboutDialog;
+}
+
+class AboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AboutDialog(QWidget* parent, const librepcb::Version& appVersion, const QString& gitVersion);
+    ~AboutDialog();
+
+private:
+    Ui::AboutDialog *ui;
+};
+
+#endif // ABOUTDIALOG_H
